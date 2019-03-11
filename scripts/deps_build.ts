0633f59074c45b6bb92c20068a37a95a9671b028
Colden Cullen <colden@amazon.com>
2019-03-11 12:39:13 -0700

    Add quotes

--- scripts/deps_build.ts
+++ scripts/deps_build.ts
@@ -151,7 +151,7 @@ async function build_dependency(lib_name: string, ...cmake_args: string[]) {
         'cmake',
         await get_generator_string(),
         cross_compile_string,
-        '-DCMAKE_PREFIX_PATH=' + [dep_install_path, node_install_path].join(';'),
+        '"-DCMAKE_PREFIX_PATH=' + [dep_install_path, node_install_path].join(';') + '"',
         '-DCMAKE_INSTALL_PREFIX=' + dep_install_path,
         '-DBUILD_SHARED_LIBS=OFF',
         '-DCMAKE_INSTALL_LIBDIR=' + lib_dir,
