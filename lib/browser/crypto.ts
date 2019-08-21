817d10c9c9936f677f5d898b1b9909c0d560550e
Justin Boswell <boswej@amazon.com>
2019-08-21 10:00:12 -0700

    Added missing crypto function to browser, and fixed up one-shot unit tests

--- lib/browser/crypto.ts
+++ lib/browser/crypto.ts
@@ -33,6 +33,14 @@ export class Md5Hash {
     }
 }
 
+
+/**
+ * Computes an MD5 hash. Use this if you don't need to stream the data you're hashing and can load the entire input
+ * into memory.
+ *
+ * @param data The data to hash
+ * @param truncate_to The maximum number of bytes to receive. Leave as undefined or 0 to receive the entire digest.
+ */
 export function hash_md5(data: Hashable, truncate_to?: number): DataView {
     const md5 = new Md5Hash();
     md5.update(data);
@@ -60,6 +68,29 @@ export class Sha256Hmac {
     }
 }
 
+/**
+ * Computes an SHA256 hash. Use this if you don't need to stream the data you're hashing and can load the entire input
+ * into memory.
+ *
+ * @param data The data to hash
+ * @param truncate_to The maximum number of bytes to receive. Leave as undefined or 0 to receive the entire digest.
+ */
+export function hash_sha256(data: Hashable, truncate_to?: number): DataView {
+    const digest = Crypto.SHA256(data.toString()).toString();
+    const truncated = digest.substring(0, truncate_to ? truncate_to : digest.length);
+    const encoder = new TextEncoder();
+    const bytes = encoder.encode(truncated);
+    return new DataView(bytes.buffer);
+}
+
+/**
+ * Computes an SHA256 HMAC. Use this if you don't need to stream the data you're hashing and can load the entire input
+ * into memory.
+ *
+ * @param secret The key to use for the HMAC process
+ * @param data The data to hash
+ * @param truncate_to The maximum number of bytes to receive. Leave as undefined or 0 to receive the entire digest.
+ */
 export function hmac_sha256(secret: Hashable, data: Hashable, truncate_to?: number): DataView {
     const hmac = new Sha256Hmac(secret);
     hmac.update(data);
