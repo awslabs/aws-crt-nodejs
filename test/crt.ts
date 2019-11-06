c8626921bc8198412b99c53b2434dcdf462220d2
Justin Boswell <boswej@amazon.com>
2019-11-06 14:50:29 -0800

    Actually expose and test native_memory/native_memory_dump

---
+++ test/crt.ts
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+ // Force memory tracing on for this suite
+process.env = Object.assign(process.env, { 'AWS_SDK_MEMORY_TRACING': '2' });
+
+import { crt } from '../lib/index';
+
+test('Native Memory', () => {
+    // Typical values seem to be around ~128K
+    expect(crt.native_memory()).toBeLessThan(256 * 1024);
+    expect(crt.native_memory()).toBeGreaterThan(0);
+});
+
