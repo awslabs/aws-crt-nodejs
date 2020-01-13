0f75f36eb99eeac2394b8aa6767ca312e57a4031
Justin Boswell <boswej@amazon.com>
2020-01-13 13:56:25 -0800

    Updated aws-common-runtime deps to latest releases

--- source/io.c
+++ source/io.c
@@ -167,7 +167,12 @@ napi_value aws_napi_io_client_bootstrap_new(napi_env env, napi_callback_info inf
         goto clean_up;
     }
 
-    binding->bootstrap = aws_client_bootstrap_new(allocator, aws_napi_get_node_elg(), &binding->resolver, NULL);
+    struct aws_client_bootstrap_options options = {
+        .event_loop_group = aws_napi_get_node_elg(),
+        .host_resolver = &binding->resolver,
+    };
+
+    binding->bootstrap = aws_client_bootstrap_new(allocator, &options);
     if (!binding->bootstrap) {
         napi_throw_error(env, NULL, "Failed init client_bootstrap");
         goto clean_up;
