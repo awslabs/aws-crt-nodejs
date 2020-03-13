b1f64300b63b1d7ecc23317e92104c8d23240ea9
Justin Boswell <boswej@amazon.com>
2020-03-12 17:48:26 -0700

    Try this

--- scripts/install.js
+++ scripts/install.js
@@ -32,6 +32,8 @@ var buildSystem = new cmake.BuildSystem({
         CMAKE_EXPORT_COMPILE_COMMANDS: true,
         CMAKE_JS_PLATFORM: os.platform,
         CMAKE_JS_ARCH: os.arch,
+        LibCrypto_INCLUDE_DIR: path.join(path.dirname(process.argv0), 'include', 'node'),
+        LibCrypto_SHARED_LIBRARY: process.execPath
     },
 });
 buildSystem.build();
