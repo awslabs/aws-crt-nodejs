eb6dff01b99bac6c90c719d3d2624f8fee31f818
Justin Boswell <boswej@amazon.com>
2020-05-29 02:29:25 +0000

    Fixed jest-puppeteer config, disabled sandbox

--- test/browser/jest.setup.js
+++ test/browser/jest.setup.js
@@ -3,5 +3,8 @@ const { setup: setupPuppeteer } = require('jest-environment-puppeteer');
  * Sets up the environment for running tests with Jest + Puppeteer
  */
 module.exports = async function globalSetup(globalConfig) {
+    globalConfig.launch = {
+        args: ['--no-sandbox']
+    }
     await setupPuppeteer(globalConfig);
 };
