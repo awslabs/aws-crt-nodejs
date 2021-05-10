fce788b9669c7ef1f984d595764693a59c73f248
Bret Ambrose <bretambrose@gmail.com>
2021-05-10 16:54:16 -0700

    Integration test setup (#210)
    * Integration build spec based on python
    * Audit + directory debugging
    * Update auth to working ecs provider

---
+++ codebuild/linux-integration-tests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -ex
+
+if test -f "/tmp/setup_proxy_test_env.sh"; then
+    source /tmp/setup_proxy_test_env.sh
+fi
+
+env
+
+git submodule update --init
+
+# build package
+cd $CODEBUILD_SRC_DIR
+
+export AWS_CRT_MEMORY_TRACING=2
+
+npm install
+npm run install
+npm test
+
