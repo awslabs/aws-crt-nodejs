58ba0b1c37cb8e6c61dfc8bb33d4b8388240a0c2
Dengke Tang <dengket@amazon.com>
2021-09-16 10:30:52 -0700

    better error handling

--- scripts/build.js
+++ scripts/build.js
@@ -12,7 +12,7 @@ const cmake = require("cmake-js");
 const axios = require("axios");
 const tar = require('tar');
 
-async function downloadFile(fileUrl, outputLocationPath) {
+function downloadFile(fileUrl, outputLocationPath) {
     const writer = fs.createWriteStream(outputLocationPath);
     return axios({
         method: 'get',
@@ -36,7 +36,7 @@ async function downloadFile(fileUrl, outputLocationPath) {
     });
 }
 
-async function checkChecksum(url, local_file) {
+function checkChecksum(url, local_file) {
     return axios({
         method: 'get',
         url: url,
@@ -67,19 +67,11 @@ async function checkChecksum(url, local_file) {
 
 async function fetchNativeCode(url, version, path) {
     const sourceURL = `${url}/aws-crt-${version}-source.tgz`
-    const tarballPath = path + "source.tgz"
-    return new Promise((resolve, reject) => {
-        downloadFile(sourceURL, tarballPath).then(() => {
-            // Download checksum
-            const sourceChecksumURL = `${url}/aws-crt-${version}-source.sha256`
-            checkChecksum(sourceChecksumURL, tarballPath)
-            tar.x({ file: tarballPath, strip: 2, C: nativeSourceDir })
-                .then(() => { resolve(); })
-                .catch((err) => { reject(err); })
-        }).catch((err) => {
-            reject(err)
-        })
-    });
+    const tarballPath = path + "source.tgz";
+    await downloadFile(sourceURL, tarballPath);
+    const sourceChecksumURL = `${url}/aws-crt-${version}-source.sha256`;
+    await checkChecksum(sourceChecksumURL, tarballPath);
+    await tar.x({ file: tarballPath, strip: 2, C: nativeSourceDir });
 }
 
 function buildLocally() {
@@ -109,37 +101,54 @@ function buildLocally() {
     return buildSystem.build();
 }
 
+async function buildFromRemoteSource(tmpPath) {
+    if (fs.existsSync(nativeSourceDir)) {
+        //teardown the local source code
+        fs.rmSync(nativeSourceDir, { recursive: true });
+    }
+    fs.mkdirSync(tmpPath);
+    fs.mkdirSync(nativeSourceDir);
+    // AWS common runtime aws-crt-nodejs cloudfront distribution.
+    let host = "https://d332vdhbectycy.cloudfront.net";
+    if (process.env.CRT_BINARY_HOST) {
+        // Use the host specified by user
+        host = process.env.CRT_BINARY_HOST;
+    }
+    let rawData = fs.readFileSync('package.json');
+    let package = JSON.parse(rawData);
+    const version = package["version"];
+    await fetchNativeCode(host, version, tmpPath);
+    // Clean up temp directory
+    fs.rmSync(tmpPath, { recursive: true });
+    // Kick off local build
+    await buildLocally();
+    // Local build finished successfully, we don't need source anymore.
+    fs.rmSync(nativeSourceDir, { recursive: true });
+}
+
+function checkDoDownload() {
+    if (!fs.existsSync(".git/") || process.argv.includes('--source_download')) {
+        return true;
+    }
+    return false;
+}
+
 // Makes sure the work directory is what we need
 const workDir = path.join(__dirname, "../")
 process.chdir(workDir);
 const nativeSourceDir = "crt/"
 
-if (!fs.existsSync(nativeSourceDir)) {
+if (checkDoDownload()) {
     const tmpPath = path.join(__dirname, `temp${crypto.randomBytes(16).toString("hex")}/`);
-    fs.mkdirSync(tmpPath);
-    fs.mkdirSync(nativeSourceDir);
-
-    // There is no native code, we are not building from source.
-    (async () => {
-        // AWS common runtime aws-crt-nodejs cloudfront distribution.
-        let host = "https://d332vdhbectycy.cloudfront.net";
-        if (process.env.CRT_BINARY_HOST) {
-            // Use the host specified by user
-            host = process.env.CRT_BINARY_HOST;
-        }
-        let rawData = fs.readFileSync('package.json');
-        let package = JSON.parse(rawData);
-        const version = package["version"];
-        fetchNativeCode(host, version, tmpPath).then(() => {
-            // Clean up temp directory
-            fs.rmSync(tmpPath, { recursive: true });
-            // Kick off local build
-            buildLocally().then(() => {
-                // Local build finished successfully, we don't need source anymore.
-                fs.rmSync(nativeSourceDir, { recursive: true });
-            })
-        })
-    })();
+    try {
+        buildFromRemoteSource(tmpPath);
+    }
+    catch (err) {
+        // teardown tmpPath and source directory on failure
+        fs.rmSync(tmpPath, { recursive: true });
+        fs.rmSync(nativeSourceDir, { recursive: true });
+        throw err;
+    }
 } else {
     // Kick off local build
     buildLocally();
