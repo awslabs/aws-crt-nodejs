5f5310b30d1afc2e5f23e2968eda622e57627943
Dengke Tang <dengket@amazon.com>
2021-09-16 17:06:20 -0700

    more fix

--- continuous-delivery/publish.sh
+++ continuous-delivery/publish.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -ex
 
-npm_pack_file=$(ls | grep -E 'aws-crt-[0-9.]*.tgz')
-echo $npm_pack_file
-npm --userconfig ./.npmrc publish $npm_pack_file
+# now get the tag
+CURRENT_TAG=$(git describe --tags | cut -f2 -dv)
+
+cd $CODEBUILD_SRC_DIR_aws_crt_nodejs_packed
+aws secretsmanager get-secret-value --secret-id prod/npm-registry/.npmrc --region us-east-1 | jq -r .SecretString > .npmrc
+
+npm --userconfig ./.npmrc publish aws-crt-$CURRENT_TAG_VERSION.tgz
