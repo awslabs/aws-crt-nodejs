e5bac4fbde30471bffaa8f72dac996a03c2c8836
Bret Ambrose <bretambrose@gmail.com>
2021-10-21 16:34:06 -0700

    Better way to address mqtt and aws-sdk webpack issues

--- samples/browser/pub_sub/webpack.config.js
+++ samples/browser/pub_sub/webpack.config.js
@@ -1,3 +1,5 @@
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
+
 module.exports = {
     entry: "./index.ts",
     devtool: "source-map",
@@ -6,20 +8,15 @@ module.exports = {
         filename: "index.js"
     },
     resolve: {
-        extensions: [".tsx", ".ts", ".js", ".json"],
-        // webpack 4 used to have this as a dependency but now webpack 5 (which has to be used from a security
-        // standpoint) does not.  This in turn breaks the aws-sdk dependency's 'util' resolution, as 'util' was
-        // an implicit dependency of the SDK via the webpack (4) dependency.
-        // To fix it, we add a technically unnecessary dependency on 'util' in this project and then update the
-        // webpack resolution fallback rules to map aws-sdk's 'util' imports to the installed version of 'util'
-        fallback: {
-            "util": require.resolve("util/")
-        }
+        extensions: [".tsx", ".ts", ".js", ".json"]
     },
     module: {
         rules: [
             // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
             { test: /\.tsx?$/, use: ["ts-loader"], exclude: /node_modules/ }
         ]
-    }
+    },
+    plugins: [
+        new NodePolyfillPlugin()
+    ]
 }
