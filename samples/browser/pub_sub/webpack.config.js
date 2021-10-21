a85dba643c273f2b7dabfa142e2446a08d79b77f
Bret Ambrose <bretambrose@gmail.com>
2021-10-21 13:21:53 -0700

    Use the right util

--- samples/browser/pub_sub/webpack.config.js
+++ samples/browser/pub_sub/webpack.config.js
@@ -7,8 +7,13 @@ module.exports = {
     },
     resolve: {
         extensions: [".tsx", ".ts", ".js", ".json"],
+        // webpack 4 used to have this as a dependency but now webpack 5 (which has to be used from a security
+        // standpoint) does not.  This in turn breaks the aws-sdk dependency's 'util' resolution, as 'util' was
+        // an implicit dependency of the SDK via the webpack (4) dependency.
+        // To fix it, we add a technically unnecessary dependency on 'util' in this project and then update the
+        // webpack resolution fallback rules to map aws-sdk's 'util' imports to the installed version of 'util'
         fallback: {
-            util$: './util.js',
+            "util": require.resolve("util/")
         }
     },
     module: {
