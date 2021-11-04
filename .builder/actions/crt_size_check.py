19347c2b9deb93868c5fb62bb31e062db35dafcb
Steve Kim <sbstevek@amazon.com>
2021-11-04 16:03:05 -0700

    crt_size_check testing

--- .builder/actions/crt_size_check.py
+++ .builder/actions/crt_size_check.py
@@ -5,20 +5,21 @@ import os
 class CrtSizeCheck(Builder.Action):
 
     def run(self, env):
-        print("CrtSizeCheck running")
-        # Code to search dist folder for aws-crt-nodejs.node file location
-        dist_folder_path = '../../dist'
-        file_folder_path = dist_folder_path + '/bin/darwin-x64/'
 
-        file_path = file_folder_path + 'aws-crt-nodejs.node'
+        print('Searching for aws-crt-nodejs.node')
+
+        for root, dirs, files in os.walk(os.getcwd() + '/dist'):
+            if 'aws-crt-nodejs.node' in files:
+                file_path = os.path.join(
+                    root, 'aws-crt-nodejs.node')
+                print(file_path + ' found')
 
         if os.path.isfile(file_path):
-            print(file_path + " found")
-            if os.stat(file_path).st_size <= 12000:
-                print(file_path + " is <= 12000 bytes")
+            print(file_path + ' file size: ' + str(os.stat(file_path).st_size))
+            if os.stat(file_path).st_size <= 10000000:
+                print(file_path + " is <= 10000000 bytes")
             else:
-                print(file_path + " is > than 12000 bytes")
+                print(file_path + " is > than 10000000 bytes")
                 raise Exception('Exceeds file size limit')
         else:
-            print(file_path + " does not exist")
-            raise Exception('File not found')
+            raise Exception('aws-crt-nodejs.node not found')
