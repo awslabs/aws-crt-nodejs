adef710313165ee0629a7668aee84bf3ecc9427d
Steve Kim <sbstevek@amazon.com>
2021-11-04 16:25:01 -0700

    Final crt_size_check.py

--- .builder/actions/crt_size_check.py
+++ .builder/actions/crt_size_check.py
@@ -1,14 +1,16 @@
+"""
+Test to insure package size does not exceed limits
+"""
+
 import Builder
 import os
 
 
 class CrtSizeCheck(Builder.Action):
-
     def run(self, env):
-
         print('Searching for aws-crt-nodejs.node')
-
-        max_size = 1000000
+        # Maximum package size in bytes
+        max_size = 10000000
 
         for root, dirs, files in os.walk(os.getcwd() + '/dist'):
             if 'aws-crt-nodejs.node' in files:
@@ -21,7 +23,6 @@ class CrtSizeCheck(Builder.Action):
             if os.stat(file_path).st_size <= max_size:
                 print(file_path + " is <= " + str(max_size) + " bytes")
             else:
-                print(file_path + " is > than " + str(max_size) + " bytes")
                 raise Exception('Exceeds file size limit')
         else:
             raise Exception('aws-crt-nodejs.node not found')
