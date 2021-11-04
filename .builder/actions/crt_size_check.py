b40bc833e69993874b94be53c12bdf8062b7b302
Steve Kim <sbstevek@amazon.com>
2021-11-04 15:20:29 -0700

    continuing work on building CI testing

---
+++ .builder/actions/crt_size_check.py
@@ -0,0 +1,20 @@
+import Buider
+import os
+
+
+class CrtSizeCheck(Builder.Action):
+
+    def run(self, env):
+        print("CrtSizeCheck running")
+        file_path = r"/Volumes/workplace/ticket/aws-crt-nodejs/scripts/tsc.js"
+
+        if os.path.isfile(file_path):
+            print(file_path + " found")
+            if os.stat(file_path).st_size <= 12000:
+                print(file_path + " is <= 12000 bytes")
+            else:
+                print(file_path + " is > than 12000 bytes")
+                raise Exception('Exceeds file size limit')
+        else:
+            print(file_path + " does not exist")
+            raise Exception('File not found')
