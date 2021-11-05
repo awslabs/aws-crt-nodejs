9d69aa2e15d93cbf35549a473184ec288dbd89dc
Steve Kim <sbstevek@amazon.com>
2021-11-05 10:58:14 -0700

    Formatted print statements and assigned None to file_path

--- .builder/actions/crt_size_check.py
+++ .builder/actions/crt_size_check.py
@@ -15,6 +15,8 @@ class CrtSizeCheck(Builder.Action):
         max_dist_size = 10000000
         # Total size of files in dist folder
         total_size = 0
+        # full path of aws-crt-nodejs.node
+        file_path = None
 
         for root, dirs, files in os.walk(os.getcwd() + '/dist'):
             for f in files:
@@ -24,16 +26,16 @@ class CrtSizeCheck(Builder.Action):
             if 'aws-crt-nodejs.node' in files:
                 file_path = os.path.join(
                     root, 'aws-crt-nodejs.node')
-                print(file_path + ' found')
+                print(f"{file_path} found")
 
-        print('Total dist folder file size: ' + str(total_size) + ' bytes')
+        print(f"Total dist folder file size: {str(total_size)} bytes")
         if total_size > max_dist_size:
             raise Exception('dist folder exceeds size limit')
 
-        if os.path.isfile(file_path):
-            print(file_path + ' file size: ' + str(os.stat(file_path).st_size))
+        if file_path is not None:
+            print(f"{file_path} file size: {str(os.stat(file_path).st_size)}")
             if os.stat(file_path).st_size <= max_node_size:
-                print(file_path + ' is <= ' + str(max_node_size) + ' bytes')
+                print(f"{file_path} is <= {str(max_node_size)} bytes")
             else:
                 raise Exception('Exceeds file size limit')
         else:
