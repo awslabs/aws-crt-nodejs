fb30a1b508696aea6d82d8b88298701dae198ced
Steve Kim <sbstevek@amazon.com>
2021-11-08 17:05:36 -0800

    Fixed to check for all files that will go into NPM package

--- .builder/actions/crt_size_check.py
+++ .builder/actions/crt_size_check.py
@@ -10,27 +10,54 @@ class CrtSizeCheck(Builder.Action):
     def run(self, env):
         # Maximum package size in bytes
         max_size = 5_000_000
-        # Maximum dist folder size in bytes
+        # size of current folder
+        folder_size = 0
+        # total size in bytes
         total_size = 0
 
-        for root, dirs, files in os.walk(os.path.join(env.project.path, 'dist/native/')):
-
+        for root, dirs, files in os.walk(os.path.join(env.project.path, 'dist/bin')):
             for f in files:
-                fp = os.path.join(root, f)
-                total_size += os.path.getsize(fp)
 
-        print(f"/native files total size: {total_size} bytes")
-
-        for root, dirs, files in os.walk(os.path.join(env.project.path, 'dist/')):
-            for f in files:
                 if 'aws-crt-nodejs.node' == f:
                     fp = os.path.join(root, f)
                     print(
                         f"{fp} file size: {str(os.stat(fp).st_size)}")
-                    if os.stat(fp).st_size <= max_size:
-                        total_size += os.path.getsize(fp)
-                    else:
-                        raise Exception(f"{fp} exceeds file size limit")
+
+                fp = os.path.join(root, f)
+                folder_size += os.path.getsize(fp)
+
+        print(f"dist/bin files size: {folder_size} bytes")
+        total_size += folder_size
+        folder_size = 0
+
+        for root, dirs, files in os.walk(os.path.join(env.project.path, 'dist/browser')):
+
+            for f in files:
+                fp = os.path.join(root, f)
+                folder_size += os.path.getsize(fp)
+
+        print(f"dist/browser files size: {folder_size} bytes")
+        total_size += folder_size
+        folder_size = 0
+
+        for root, dirs, files in os.walk(os.path.join(env.project.path, 'dist/common')):
+
+            for f in files:
+                fp = os.path.join(root, f)
+                folder_size += os.path.getsize(fp)
+
+        print(f"dist/common files size: {folder_size} bytes")
+        total_size += folder_size
+        folder_size = 0
+
+        for root, dirs, files in os.walk(os.path.join(env.project.path, 'dist/native')):
+
+            for f in files:
+                fp = os.path.join(root, f)
+                folder_size += os.path.getsize(fp)
+
+        print(f"dist/native files size: {folder_size} bytes")
+        total_size += folder_size
 
         print(f"Total NPM package file size: {str(total_size)} bytes")
         if total_size > max_size:
