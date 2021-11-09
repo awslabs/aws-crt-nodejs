e5cafe892c336e60a809392252891313a6af6354
Steve Kim <86316075+sbSteveK@users.noreply.github.com>
2021-11-09 09:22:14 -0800

    Changed crt size check to only include NPM packaged files (#273)
    * Changed crt size check to only include NPM packaged files

--- .builder/actions/crt_size_check.py
+++ .builder/actions/crt_size_check.py
@@ -9,33 +9,56 @@ import os
 class CrtSizeCheck(Builder.Action):
     def run(self, env):
         # Maximum package size in bytes
-        max_node_size = 5_000_000
-        # Maximum dist folder size in bytes
-        max_dist_size = 50_000_000
-        # Size of current folder
-        current_folder_size = 0
-        # Total size of files in dist folder
+        max_size = 5_000_000
+        # size of current folder
+        folder_size = 0
+        # total size in bytes
         total_size = 0
 
-        for root, dirs, files in os.walk(os.path.join(env.project.path, 'dist/')):
-            current_folder_size = 0
+        for root, dirs, files in os.walk(os.path.join(env.project.path, 'dist/bin')):
+            for f in files:
+
+                if 'aws-crt-nodejs.node' == f:
+                    fp = os.path.join(root, f)
+                    print(
+                        f"{fp} file size: {str(os.stat(fp).st_size)}")
+
+                fp = os.path.join(root, f)
+                folder_size += os.path.getsize(fp)
+
+        print(f"dist/bin files size: {folder_size} bytes")
+        total_size += folder_size
+        folder_size = 0
+
+        for root, dirs, files in os.walk(os.path.join(env.project.path, 'dist/browser')):
 
             for f in files:
                 fp = os.path.join(root, f)
-                current_folder_size += os.path.getsize(fp)
+                folder_size += os.path.getsize(fp)
 
-                if 'aws-crt-nodejs.node' == f:
-                    print(
-                        f"NODE FOUND: {fp} file size: {str(os.stat(fp).st_size)}")
-                    if os.stat(fp).st_size <= max_node_size:
-                        print(f"{fp} is <= {str(max_node_size)} bytes")
-                    else:
-                        raise Exception(f"{fp} exceeds file size limit")
+        print(f"dist/browser files size: {folder_size} bytes")
+        total_size += folder_size
+        folder_size = 0
 
-            if current_folder_size > 0:
-                print(f"{root} = {str(current_folder_size)} bytes")
-                total_size += current_folder_size
+        for root, dirs, files in os.walk(os.path.join(env.project.path, 'dist/common')):
 
-        print(f"Total /dist folder file size: {str(total_size)} bytes")
-        if total_size > max_dist_size:
-            raise Exception('/dist folder exceeds size limit')
+            for f in files:
+                fp = os.path.join(root, f)
+                folder_size += os.path.getsize(fp)
+
+        print(f"dist/common files size: {folder_size} bytes")
+        total_size += folder_size
+        folder_size = 0
+
+        for root, dirs, files in os.walk(os.path.join(env.project.path, 'dist/native')):
+
+            for f in files:
+                fp = os.path.join(root, f)
+                folder_size += os.path.getsize(fp)
+
+        print(f"dist/native files size: {folder_size} bytes")
+        total_size += folder_size
+
+        print(f"Total NPM package file size: {str(total_size)} bytes")
+        if total_size > max_size:
+            raise Exception('NPM package exceeds size limit')
