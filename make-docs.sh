aee5eaebdbd5a4142c5fcc74942177d1d526b3eb
Michael Graeb <graebm@amazon.com>
2022-04-18 10:22:19 -0700

    move files related to docs generation into docsrc/ this is how we do it in our other repos

--- make-docs.sh
+++ make-docs.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
+set -e
 
 pushd `dirname $0` > /dev/null
 
-npx typedoc --options ./typedoc-node.json
-npx typedoc --options ./typedoc-browser.json
+# clean
+rm -rf docs/
+
+# build
+npx typedoc --options ./docsrc/typedoc-node.json
+npx typedoc --options ./docsrc/typedoc-browser.json
+cp docsrc/index.html docs/index.html
 
 popd > /dev/null
