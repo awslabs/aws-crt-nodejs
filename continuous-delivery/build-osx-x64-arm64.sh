9552f96f7e08ae8b62252b4bb230484b70e66f97
Michael Graeb <graebm@amazon.com>
2022-05-05 10:58:29 -0700

    check cross-compile from CI add a bunch of comments

--- continuous-delivery/build-osx-x64-arm64.sh
+++ continuous-delivery/build-osx-x64-arm64.sh
@@ -6,4 +6,7 @@ set -ex
 npm install --unsafe-perm
 
 # build arm64
+# NOTE: This is the only build job that compiles for two different architectures.
+# Our release pipeline doesn't currently have an arm64 OSX machine,
+# so we use the x64 OSX machine create both types of binaries.
 node ./scripts/build --force-arch arm64
