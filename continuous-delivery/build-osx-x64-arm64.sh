72d2a3960d2613c35498e4b588786d7616c17995
Michael Graeb <graebm@amazon.com>
2022-05-05 17:12:51 -0700

    trivial cleanup

--- continuous-delivery/build-osx-x64-arm64.sh
+++ continuous-delivery/build-osx-x64-arm64.sh
@@ -8,5 +8,5 @@ npm install --unsafe-perm
 # build arm64
 # NOTE: This is the only build job that compiles for two different architectures.
 # Our release pipeline doesn't currently have an arm64 OSX machine,
-# so we use the x64 OSX machine create both types of binaries.
+# so we use the x64 OSX machine to create both types of binaries.
 node ./scripts/build --target-arch arm64
