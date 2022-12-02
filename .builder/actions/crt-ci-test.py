27202bb177506ed3259ad2d62371a81e51fb308b
Bret Ambrose <bretambrose@gmail.com>
2022-12-02 11:17:44 -0800

    Rework temp file management to account for Windows inability to read from open temp files

--- .builder/actions/crt-ci-test.py
+++ .builder/actions/crt-ci-test.py
@@ -21,25 +21,34 @@ class CrtCiTest(Builder.Action):
     def _write_secret_to_temp_file(self, env, secret_name):
         secret_value = env.shell.get_secret(secret_name)
 
-        temp_file = tempfile.NamedTemporaryFile()
-        temp_file.write(str.encode(secret_value))
-        temp_file.flush()
+        fd, filename = tempfile.mkstemp()
+        os.write(fd, str.encode(secret_value))
+        os.close(fd)
 
-        return temp_file
+        return filename
 
     def run(self, env):
         env.shell.setenv("AWS_TESTING_COGNITO_IDENTITY", env.shell.get_secret("aws-c-auth-testing/cognito-identity"))
 
         self._write_environment_script_secret_to_env(env, "mqtt5-testing/github-ci-environment")
 
-        with self._write_secret_to_temp_file(env, "unit-test/certificate") as cert_file, self._write_secret_to_temp_file(env, "unit-test/privatekey") as key_file:
+        # Unfortunately, we can't use NamedTemporaryFile and a with-block because NamedTemporaryFile is not readable
+        # on Windows.
+        try:
+            cert_file_name = self._write_secret_to_temp_file(env, "unit-test/certificate")
+            key_file_name = self._write_secret_to_temp_file(env, "unit-test/privatekey")
 
-            env.shell.setenv("AWS_TEST_MQTT5_IOT_CORE_CERTIFICATE_PATH", cert_file.name)
-            env.shell.setenv("AWS_TEST_MQTT5_IOT_CORE_KEY_PATH", key_file.name)
+            env.shell.setenv("AWS_TEST_MQTT5_IOT_CORE_CERTIFICATE_PATH", cert_file_name)
+            env.shell.setenv("AWS_TEST_MQTT5_IOT_CORE_KEY_PATH", key_file_name)
 
             if os.system("npm run test:native"):
                 # Failed
                 actions.append("exit 1")
+        finally:
+            if cert_file_name:
+                os.remove(cert_file_name)
+            if key_file_name:
+                os.remove(key_file_name)
 
         actions = []
 
