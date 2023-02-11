3985d8f08670663c378edcbcbe5096664726713a
Noah Beard <ncbeard@amazon.com>
2023-02-11 09:54:23 -0500

    Adjust connection failed test

--- lib/browser/aws_iot.spec.ts
+++ lib/browser/aws_iot.spec.ts
@@ -44,28 +44,22 @@ test_utils.conditional_test(test_utils.ClientEnvironmentalConfig.hasIotCoreEnvir
 });
 
 test_utils.conditional_test(test_utils.ClientEnvironmentalConfig.hasIotCoreEnvironment())('Aws Iot Core Mqtt 311 over websockets with environmental credentials - Connection Failure', async () => {
-    let provider: auth.StaticCredentialProvider = new auth.StaticCredentialProvider({
-        aws_access_id: test_utils.ClientEnvironmentalConfig.AWS_IOT_ACCESS_KEY_ID,
-        aws_secret_key: test_utils.ClientEnvironmentalConfig.AWS_IOT_SECRET_ACCESS_KEY,
-        aws_region: "us-east-1"
-    });
-
     let builder = aws_iot_mqtt311.AwsIotMqttConnectionConfigBuilder.new_builder_for_websocket();
     builder.with_endpoint(test_utils.ClientEnvironmentalConfig.AWS_IOT_HOST);
-    builder.with_credential_provider(provider);
-    builder.with_keep_alive_seconds(1200);
+    builder.with_keep_alive_seconds(1);
     builder.with_client_id(`client-${uuid()}`);
-    // Use the wrong port to make it fail
-    builder.with_port(123);
+    // Do not define a credentials provider AND use the wrong port to make it fail
+    builder.with_port(321);
 
     let client = new mqtt311.MqttClient();
     let connection = client.new_connection(builder.build());
 
     const connectionFailure = once(connection, "connection_failure")
 
-    connection.connect();
-    console.log("About to wait for connection failure event...");
+    let connectResult = connection.connect();
+    await expect(connectResult).rejects.toBeDefined();
+
     let connectionFailedEvent: mqtt311.OnConnectionFailedResult = (await connectionFailure)[0];
-    console.log("Got connection failure event...");
+    expect(connectionFailedEvent).toBeDefined();
     expect(connectionFailedEvent.error).toBeDefined();
 });
