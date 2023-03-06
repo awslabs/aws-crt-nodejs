d9e7895afa192f214600ba2acd82c4be34ff840d
Bret Ambrose <bretambrose@gmail.com>
2023-03-06 13:19:17 -0800

    First draft

--- .builder/actions/crt-ci-test.py
+++ .builder/actions/crt-ci-test.py
@@ -27,14 +27,54 @@ class CrtCiTest(Builder.Action):
 
         return filename
 
-    def run(self, env):
-        env.shell.setenv("AWS_TESTING_COGNITO_IDENTITY", env.shell.get_secret("aws-c-auth-testing/cognito-identity"), quiet=True)
+    def _build_and_run_eventstream_echo_server(self, env):
+        java_sdk_dir = None
+        proc = None
 
-        self._write_environment_script_secret_to_env(env, "mqtt5-testing/github-ci-environment")
+        try:
+            env.shell.exec(["mvn", "--version"])
+
+            # maven is installed, so this is a configuration we can start an event stream echo server
+            java_sdk_dir = env.shell.mktemp()
+
+            env.shell.exec(["git", "clone", "https://github.com/aws/aws-iot-device-sdk-java-v2"], working_dir=java_sdk_dir)
+
+            sdk_dir = os.path.join(java_sdk_dir, "aws-iot-device-sdk-java-v2", "sdk")
+            env.shell.pushd(sdk_dir)
+
+            try:
+                env.shell.exec(["mvn", "compile"])
+                env.shell.exec(["mvn", "test", "-DskipTests=true"])
+                env.shell.exec(["mvn", "dependency:build-classpath", "-Dmdep.outputFile=classpath.txt"])
+
+                with open('classpath.txt', 'r') as file:
+                    classpath = file.read()
+
+                echo_server_command = ["java", "-classpath", f"{sdk_dir}/target/test-classes:$CURRENT_DIR/target/classes:{classpath}", "software.amazon.awssdk.eventstreamrpc.echotest.EchoTestServiceRunner", "127.0.0.1", "8033"]
+
+                # bypass builder's exec wrapper since it doesn't allow for background execution
+                proc = subprocess.Popen(echo_server_command)
+
+                env.shell.setenv("AWS_TEST_EVENT_STREAM_ECHO_SERVER_HOST", "127.0.0.1", quiet=False)
+                env.shell.setenv("AWS_TEST_EVENT_STREAM_ECHO_SERVER_PORT", "8033", quiet=False)
+            finally:
+                env.shell.popd()
+
+        return (proc, java_sdk_dir)
+
+    def run(self, env):
 
         # Unfortunately, we can't use NamedTemporaryFile and a with-block because NamedTemporaryFile is not readable
         # on Windows.
         try:
+
+            (proc, java_sdk_dir) = self._build_and_run_eventstream_echo_server(env)
+
+            env.shell.setenv("AWS_TESTING_COGNITO_IDENTITY", env.shell.get_secret("aws-c-auth-testing/cognito-identity"), quiet=True)
+
+            self._write_environment_script_secret_to_env(env, "mqtt5-testing/github-ci-environment")
+
+
             cert_file_name = self._write_secret_to_temp_file(env, "unit-test/certificate")
             key_file_name = self._write_secret_to_temp_file(env, "unit-test/privatekey")
 
@@ -45,10 +85,15 @@ class CrtCiTest(Builder.Action):
                 # Failed
                 actions.append("exit 1")
         finally:
+            if proc:
+                proc.terminate()
+                proc.wait()
             if cert_file_name:
                 os.remove(cert_file_name)
             if key_file_name:
                 os.remove(key_file_name)
+            if java_sdk_dir:
+                env.shell.rm(java_sdk_dir)
 
         actions = []
 
