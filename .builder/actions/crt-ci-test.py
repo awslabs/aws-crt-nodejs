0f4f9fafdb6a4817612ee5d78f8f4dce1eb85b34
Bret Ambrose <bretambrose@gmail.com>
2023-03-06 14:47:10 -0800

    Fix a bunch of stuff

--- .builder/actions/crt-ci-test.py
+++ .builder/actions/crt-ci-test.py
@@ -32,25 +32,30 @@ class CrtCiTest(Builder.Action):
         proc = None
 
         try:
-            env.shell.exec(["mvn", "--version"], check=true)
+            env.shell.exec(["mvn", "--version"], check=True)
 
             # maven is installed, so this is a configuration we can start an event stream echo server
             java_sdk_dir = env.shell.mktemp()
 
-            env.shell.exec(["git", "clone", "https://github.com/aws/aws-iot-device-sdk-java-v2"], working_dir=java_sdk_dir, check=true)
+            env.shell.exec(["git", "clone", "https://github.com/aws/aws-iot-device-sdk-java-v2"], working_dir=java_sdk_dir, check=True)
 
             sdk_dir = os.path.join(java_sdk_dir, "aws-iot-device-sdk-java-v2", "sdk")
             env.shell.pushd(sdk_dir)
 
             try:
-                env.shell.exec(["mvn", "compile"], check=true)
-                env.shell.exec(["mvn", "test", "-DskipTests=true"], check=true)
-                env.shell.exec(["mvn", "dependency:build-classpath", "-Dmdep.outputFile=classpath.txt"], check=true)
+                env.shell.exec(["mvn", "compile"], check=True)
+                env.shell.exec(["mvn", "test", "-DskipTests=true"], check=True)
+                env.shell.exec(["mvn", "dependency:build-classpath", "-Dmdep.outputFile=classpath.txt"], check=True)
 
                 with open('classpath.txt', 'r') as file:
                     classpath = file.read()
 
-                echo_server_command = ["java", "-classpath", f"{sdk_dir}/target/test-classes:{sdk_dir}/target/classes:{classpath}", "software.amazon.awssdk.eventstreamrpc.echotest.EchoTestServiceRunner", "127.0.0.1", "8033"]
+                test_class_path = os.path.join(sdk_dir, "target", "test-classes")
+                target_class_path = os.path.join(sdk_dir, "target", "classes")
+
+                echo_server_command = ["java", "-classpath", f"{test_class_path}:{target_class_path}:{classpath}", "software.amazon.awssdk.eventstreamrpc.echotest.EchoTestServiceRunner", "127.0.0.1", "8033"]
+
+                print(f'Echo server command: {echo_server_command}')
 
                 # bypass builder's exec wrapper since it doesn't allow for background execution
                 proc = subprocess.Popen(echo_server_command)
@@ -66,6 +71,10 @@ class CrtCiTest(Builder.Action):
         return proc, java_sdk_dir
 
     def run(self, env):
+
+        actions = []
+        proc = None
+        java_sdk_dir = None
 
         # Unfortunately, we can't use NamedTemporaryFile and a with-block because NamedTemporaryFile is not readable
         # on Windows.
@@ -83,9 +92,10 @@ class CrtCiTest(Builder.Action):
             env.shell.setenv("AWS_TEST_MQTT5_IOT_CORE_CERTIFICATE_PATH", cert_file_name, quiet=True)
             env.shell.setenv("AWS_TEST_MQTT5_IOT_CORE_KEY_PATH", key_file_name, quiet=True)
 
-            if os.system("npm run test:native"):
-                # Failed
-                actions.append("exit 1")
+            env.shell.exec(["npm", "run", "test:native"])
+        except:
+            print(f'Exception while running tests')
+            raise
         finally:
             if proc:
                 proc.terminate()
@@ -96,7 +106,5 @@ class CrtCiTest(Builder.Action):
                 os.remove(key_file_name)
             if java_sdk_dir:
                 env.shell.rm(java_sdk_dir)
-
-        actions = []
 
         return Builder.Script(actions, name='crt-ci-test')
