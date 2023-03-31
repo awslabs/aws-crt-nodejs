9d5ce07d9d88b817f866c8b7afdbab7a09fa0125
Noah Beard <ncbeard@amazon.com>
2023-03-31 16:12:47 -0400

    Add angular support to browser polyfil

--- samples/browser/angular_pub_sub/src/main.ts
+++ samples/browser/angular_pub_sub/src/main.ts
@@ -1,4 +1,3 @@
-import "./polyfil"
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
--- samples/browser/angular_pub_sub/src/polyfil.ts
+++
@@ -1,4 +0,0 @@
-// Add shim for Global to make Node libraries work.
-// https://github.com/angular/angular-cli/issues/9827#issuecomment-386154063
-(window as any).global = window;
-export {}
