2aa2123991f144df70cdaca1272610fe80435a10
Noah Beard <ncbeard@amazon.com>
2023-03-31 16:22:40 -0400

    Remove config

--- samples/browser/angular_pub_sub/src/app/config.ts
+++ samples/browser/angular_pub_sub/src/app/config.ts
@@ -1,7 +1,3 @@
-// export const AWS_REGION = '<your region>';
-// export const AWS_COGNITO_IDENTITY_POOL_ID = '<your identity pool id>';
-// export const AWS_IOT_ENDPOINT = '<your endpoint>';
-
-export const AWS_REGION = 'us-east-1';
-export const AWS_COGNITO_IDENTITY_POOL_ID = 'us-east-1:0ecd95c3-cbae-4304-b10a-3313f0617541';
-export const AWS_IOT_ENDPOINT = 'a37bosfsrcgx5c-ats.iot.us-east-1.amazonaws.com';
+export const AWS_REGION = '<your region>';
+export const AWS_COGNITO_IDENTITY_POOL_ID = '<your identity pool id>';
+export const AWS_IOT_ENDPOINT = '<your endpoint>';
