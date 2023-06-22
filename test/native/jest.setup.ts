747c39bfc929a0e97a8a48d2f4b611dc7bd55e9e
Waqar Ahmed Khan <waahm7@gmail.com>
2023-06-22 00:32:44 -0700

    Update to process.env

--- test/native/jest.setup.ts
+++ test/native/jest.setup.ts
@@ -1,12 +1,11 @@
 import * as io from "../../lib/native/io";
-import * as test_env from "../test_env";
 import {Pkcs11Lib} from "../../lib/native/io";
 
 let pkcs11_lib: io.Pkcs11Lib  | null;
 beforeAll(async () => {
     console.error("before all");
-    if(test_env.AWS_IOT_ENV.MQTT311_PKCS11_LIB_PATH !== ""){
-        pkcs11_lib = new io.Pkcs11Lib(test_env.AWS_IOT_ENV.MQTT311_PKCS11_LIB_PATH, Pkcs11Lib.InitializeFinalizeBehavior.STRICT);
+    if(process.env.AWS_TEST_PKCS11_LIB){
+        pkcs11_lib = new io.Pkcs11Lib(process.env.AWS_TEST_PKCS11_LIB, Pkcs11Lib.InitializeFinalizeBehavior.STRICT);
     }
 });
 
