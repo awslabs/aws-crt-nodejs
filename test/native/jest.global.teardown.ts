138db1b7ef8f8d7a1d0ca29b8ac3555dc13c9879
Waqar Ahmed Khan <waahm7@gmail.com>
2023-06-21 23:54:38 -0700

    try in global teardown

--- test/native/jest.global.teardown.ts
+++ test/native/jest.global.teardown.ts
@@ -1,3 +1,9 @@
+import * as io from "../../lib/native/io";
+import * as test_env from "../test_env";
+import {Pkcs11Lib} from "../../lib/native/io";
+
 export default async () => {
     console.log('\nhello, this is just before end start running');
+    const pkcs11_lib = new io.Pkcs11Lib(test_env.AWS_IOT_ENV.MQTT311_PKCS11_LIB_PATH, Pkcs11Lib.InitializeFinalizeBehavior.STRICT);
+    pkcs11_lib.close()
 };
