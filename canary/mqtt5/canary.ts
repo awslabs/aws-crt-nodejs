9433a611c48c3bed00f3b3bac68de0031a2d56bb
Zhihui Xia <zhvxia@amazon.com>
2023-11-15 10:19:48 -0800

    update canary

--- canary/mqtt5/canary.ts
+++ canary/mqtt5/canary.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
-import {ICrtError, mqtt5, mqtt5_packet} from "aws-crt";
+import {ICrtError, mqtt5} from "aws-crt";
 import {once} from "events";
 import {v4 as uuid} from "uuid";
 var weightedRandom = require('weighted-random');
@@ -13,27 +13,52 @@ type Args = { [index: string]: any };
 const yargs = require('yargs');
 
 yargs.command('*', false, (yargs: any) => {
-    yargs.option('duration', {
-        description: 'INT: time in seconds to run the canary',
-        type: 'number',
-        default: 3600,
-    })
+    yargs.option({
+        'duration': {
+            description: 'INT: time in seconds to run the canary',
+            type: 'number',
+            default: 120,
+        },
+        'endpoint': {
+            description: 'STR: endpoint to connect to',
+            type: 'string',
+            default: 'localhost',
+        },
+        'port': {
+            description: 'number: port to connect to',
+            type: 'number',
+            default: 1883,
+        },
+        'tps': {
+            description: 'number: transaction per second',
+            type: 'number',
+            default: 0,
+        }
+    });
 }, main).parse();
 
 let RECEIVED_TOPIC : string = "Canary/Received/Topic";
 
 interface CanaryMqttStatistics {
-    clientsUsed : number;
+    clientsUsed: number;
     publishesReceived: number;
-    subscribesAttempted : number;
-    subscribesSucceeded : number;
-    subscribesFailed : number;
-    unsubscribesAttempted : number;
-    unsubscribesSucceeded : number;
-    unsubscribesFailed : number;
-    publishesAttempted : number;
-    publishesSucceeded : number;
-    publishesFailed : number;
+    subscribesAttempted: number;
+    subscribesSucceeded: number;
+    subscribesFailed: number;
+    unsubscribesAttempted: number;
+    unsubscribesSucceeded: number;
+    unsubscribesFailed: number;
+    publishesAttempted: number;
+    publishesSucceeded: number;
+    publishesFailed: number;
+    totalOperation: number;
+}
+
+interface TestContext {
+    duration: number;
+    hostname: string;
+    port: number;
+    tps_sleep_time: number;
 }
 
 interface CanaryContext {
@@ -44,16 +69,20 @@ interface CanaryContext {
     subscriptions: string[];
 }
 
-function createCanaryClient(mqttStats : CanaryMqttStatistics) : mqtt5.Mqtt5Client {
+function sleep(millisecond: number) {
+    return new Promise((resolve) => setInterval(resolve, millisecond));
+}
+
+function createCanaryClient(testContext: TestContext, mqttStats: CanaryMqttStatistics): mqtt5.Mqtt5Client {
     const client_config : mqtt5.Mqtt5ClientConfig = {
-        hostName : process.env.AWS_TEST_MQTT5_DIRECT_MQTT_HOST ?? "localhost",
-        port : parseInt(process.env.AWS_TEST_MQTT5_DIRECT_MQTT_PORT ?? "0")
+        hostName: testContext.hostname,
+        port: testContext.port
     };
 
     let client : mqtt5.Mqtt5Client = new mqtt5.Mqtt5Client(client_config);
 
     client.on('error', (error: ICrtError) => {});
-    client.on("messageReceived",(message: mqtt5_packet.PublishPacket) : void => {
+    client.on("messageReceived", (eventData: mqtt5.MessageReceivedEvent): void => {
         mqttStats.publishesReceived++;
     });
 
@@ -68,7 +97,7 @@ async function doSubscribe(context : CanaryContext) {
 
         await context.client.subscribe({
             subscriptions: [
-                {topicFilter: RECEIVED_TOPIC, qos: mqtt5_packet.QoS.AtLeastOnce}
+                {topicFilter: RECEIVED_TOPIC, qos: mqtt5.QoS.AtLeastOnce }
             ]
         });
 
@@ -101,7 +130,7 @@ async function doUnsubscribe(context : CanaryContext) {
     }
 }
 
-async function doPublish(context : CanaryContext, qos: mqtt5_packet.QoS) {
+async function doPublish(context : CanaryContext, qos: mqtt5.QoS) {
     try {
         context.mqttStats.publishesAttempted++;
 
@@ -110,7 +139,7 @@ async function doPublish(context : CanaryContext, qos: mqtt5_packet.QoS) {
             qos: qos,
             payload: Buffer.alloc(10000),
             retain: false,
-            payloadFormat: mqtt5_packet.PayloadFormatIndicator.Utf8,
+            payloadFormat: mqtt5.PayloadFormatIndicator.Utf8,
             messageExpiryIntervalSeconds: 60,
             responseTopic: "talk/to/me",
             correlationData: Buffer.alloc(3000),
@@ -126,10 +155,10 @@ async function doPublish(context : CanaryContext, qos: mqtt5_packet.QoS) {
     }
 }
 
-async function runCanaryIteration(endTime: Date, mqttStats : CanaryMqttStatistics) {
+async function runCanaryIteration(testContext: TestContext, endTime: Date, mqttStats: CanaryMqttStatistics) {
 
     let context : CanaryContext = {
-        client : createCanaryClient(mqttStats),
+        client : createCanaryClient(testContext, mqttStats),
         mqttStats : mqttStats,
         subscriptions : []
     };
@@ -151,11 +180,11 @@ async function runCanaryIteration(endTime: Date, mqttStats : CanaryMqttStatistic
 
     context.client.start();
 
-    await connectionSuccess;
+        await connectionSuccess;
 
     await context.client.subscribe({
         subscriptions: [
-            { topicFilter: RECEIVED_TOPIC, qos: mqtt5_packet.QoS.AtLeastOnce }
+            { topicFilter: RECEIVED_TOPIC, qos: mqtt5.QoS.AtLeastOnce }
         ]
     });
 
@@ -164,7 +193,8 @@ async function runCanaryIteration(endTime: Date, mqttStats : CanaryMqttStatistic
         let index : number = weightedRandom(weightedOperations);
 
         await (operationTable[index].op)();
-
+        ++context.mqttStats.totalOperation;
+        await sleep(testContext.tps_sleep_time);
         currentTime = new Date();
     }
 
@@ -177,16 +207,16 @@ async function runCanaryIteration(endTime: Date, mqttStats : CanaryMqttStatistic
     context.client.close();
 }
 
-async function runCanary(durationInSeconds: number, mqttStats : CanaryMqttStatistics) {
+async function runCanary(testContext: TestContext, mqttStats: CanaryMqttStatistics) {
     let startTime: Date = new Date();
     let currentTime: Date = startTime;
     let secondsElapsed : number = 0;
     let iteration : number = 0;
 
-    while (secondsElapsed < durationInSeconds) {
-        let iterationTime : number = Math.min(durationInSeconds - secondsElapsed, 60);
+    while (secondsElapsed < testContext.duration) {
+        let iterationTime: number = Math.min(testContext.duration - secondsElapsed, 60);
         let iterationEnd = new Date(currentTime.getTime() + iterationTime * 1000);
-        await runCanaryIteration(iterationEnd, mqttStats);
+        await runCanaryIteration(testContext, iterationEnd, mqttStats);
 
         iteration++;
         console.log(`Iteration ${iteration} stats: ${JSON.stringify(mqttStats)}`);
@@ -198,22 +228,31 @@ async function runCanary(durationInSeconds: number, mqttStats : CanaryMqttStatis
 
 async function main(args : Args){
     let mqttStats : CanaryMqttStatistics = {
-        clientsUsed : 0,
+        clientsUsed: 0,
         publishesReceived: 0,
-        subscribesAttempted : 0,
-        subscribesSucceeded : 0,
-        subscribesFailed : 0,
-        unsubscribesAttempted : 0,
-        unsubscribesSucceeded : 0,
-        unsubscribesFailed : 0,
-        publishesAttempted : 0,
-        publishesSucceeded : 0,
-        publishesFailed : 0
+        subscribesAttempted: 0,
+        subscribesSucceeded: 0,
+        subscribesFailed: 0,
+        unsubscribesAttempted: 0,
+        unsubscribesSucceeded: 0,
+        unsubscribesFailed: 0,
+        publishesAttempted: 0,
+        publishesSucceeded: 0,
+        publishesFailed: 0,
+        totalOperation: 0,
     };
 
-    await runCanary(args.duration, mqttStats);
+    let testContext: TestContext = {
+        duration: args.duration,
+        hostname: args.endpoint,
+        port: args.port,
+        tps_sleep_time: args.tps == 0 ? 0 : (1000 / args.tps),
+    }
 
-    console.log(`Final Stats: ${JSON.stringify(mqttStats)}`)
+    await runCanary(testContext, mqttStats);
+
+    console.log(`Final Stats: ${JSON.stringify(mqttStats)}`);
+    console.log(`Operation TPS: ${mqttStats.totalOperation / testContext.duration}`);
 
     process.exit(0);
 
