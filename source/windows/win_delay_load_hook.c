8ea7d49350a49d31d42e729ed326c4d821cf85ae
Zhihui Xia <zhvxia@amazon.com>
2024-01-18 14:26:14 -0800

    clang-format

--- source/windows/win_delay_load_hook.c
+++ source/windows/win_delay_load_hook.c
@@ -4,22 +4,21 @@
  */
 
 /*
- * This file sets up a windows delay load hook that the dll would redirect to 
+ * This file sets up a windows delay load hook that the dll would redirect to
  * the calling process (.exe file) instead of `node.exe`.
- * 
+ *
  * This allows compiled addons to work when node.exe is renamed.
  */
 
 #ifdef _MSC_VER
 
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif
+#    ifndef WIN32_LEAN_AND_MEAN
+#        define WIN32_LEAN_AND_MEAN
+#    endif
 
-#include <windows.h>
-#include <delayimp.h>
-#include <string.h>
-
+#    include <delayimp.h>
+#    include <string.h>
+#    include <windows.h>
 
 FARPROC WINAPI delayHook(unsigned dliNotify, PDelayLoadInfo pdli) {
     switch (dliNotify) {
@@ -28,24 +27,24 @@ FARPROC WINAPI delayHook(unsigned dliNotify, PDelayLoadInfo pdli) {
             // If you want to return control to the helper, return 0.
             // Otherwise, return your own HMODULE to be used by the
             // helper instead of having it call LoadLibrary itself.
-            if (_stricmp(pdli->szDll, "node.exe") != 0)
-            {
+            if (_stricmp(pdli->szDll, "node.exe") != 0) {
                 // return control if we are not loading node.exe
                 return NULL;
             }
-            
-            // As in Electron 4.x and higher, the symbols needed by native modules are exported by `electron.exe` instead of `node.exe`. It is necessary to overwrite 
-            // the node load process. More info about Electron windows delay load issue: 
+
+            // As in Electron 4.x and higher, the symbols needed by native modules are exported by `electron.exe`
+            // instead of `node.exe`. It is necessary to overwrite the node load process. More info about Electron
+            // windows delay load issue:
             //     https://www.electronjs.org/docs/latest/tutorial/using-native-node-modules#a-note-about-win_delay_load_hook
-            
-            // If we call GetModuleHandle with NULL, GetModuleHandle returns a handle to the file used to create the calling process (.exe file). This means if we 
-            // launch the library through `node.exe`, GetModuleHandle would still return `node.exe`. While in the case of electron, instead of loading `node`, GetModuleHandle
+
+            // If we call GetModuleHandle with NULL, GetModuleHandle returns a handle to the file used to create the
+            // calling process (.exe file). This means if we launch the library through `node.exe`, GetModuleHandle
+            // would still return `node.exe`. While in the case of electron, instead of loading `node`, GetModuleHandle
             // would return `electron.exe`.
             // This would also solve the issue where the node.exe is rename.
             return (FARPROC)GetModuleHandle(NULL);
 
-        
-        // Return control in the other cases. 
+        // Return control in the other cases.
         case dliStartProcessing:
         case dliNotePreGetProcAddress:
         case dliFailLoadLib:
