b51d9c8aec28bc72761cde001c0fbaddd57963b5
Zhihui Xia <zhvxia@amazon.com>
2024-04-22 14:08:08 -0700

    test polyfills.tx

--- lib/browser/polyfills.ts
+++ lib/browser/polyfills.ts
@@ -2,16 +2,19 @@
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0.
  */
-
 import buffer from 'buffer';
 import process from 'process';
 
 // Workaround to get mqtt-js working with Webpack 5
-if (typeof window !== 'undefined') {
-    (window as any).Buffer = buffer.Buffer;
-    (window as any).process = process;
+
+if (self) {
+    (self as any).Buffer = buffer.Buffer;
+    (self as any).process = process;
+}
+
+if (self.window) {
     // NodeJS global shim workaround for Angular
     (window as any).global = window;
 }
 
-export {};
+export { };
