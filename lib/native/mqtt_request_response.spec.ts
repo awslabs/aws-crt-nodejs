e031e7e9205996edf1cde405be0cda0028118ca2
Bret Ambrose <bretambrose@gmail.com>
2024-04-22 16:02:20 -0700

    Test refactoring

--- lib/native/mqtt_request_response.spec.ts
+++ lib/native/mqtt_request_response.spec.ts
@@ -5,112 +5,180 @@
 
 
 import * as test_env from "@test/test_env"
+import * as mqtt311 from "./mqtt";
 import * as mqtt5 from "./mqtt5";
-import * as io from "./io";
 import * as mqtt_request_response from "./mqtt_request_response";
 import {v4 as uuid} from "uuid";
 import {once} from "events";
 import * as iot from "./iot";
 
-jest.setTimeout(1000000);
+jest.setTimeout(10000);
+
+enum ProtocolVersion {
+    Mqtt311,
+    Mqtt5
+}
 
 interface TestingOptions {
-    startConnected : boolean,
+    version: ProtocolVersion,
 }
 
-interface Mqtt5TestingContext {
-    protocolClient : mqtt5.Mqtt5Client,
+class TestingContext {
 
-    client : mqtt_request_response.RequestResponseClient,
-}
+    client: mqtt_request_response.RequestResponseClient;
 
-async function createMqtt5TestingContext(testOptions: TestingOptions) : Promise<Mqtt5TestingContext> {
+    private readonly init: () => Promise<void>;
+    private readonly shutdown: () => Promise<void>;
 
-    let builder = iot.AwsIotMqtt5ClientConfigBuilder.newDirectMqttBuilderWithMtlsFromPath(
-        test_env.AWS_IOT_ENV.MQTT5_HOST,
-        test_env.AWS_IOT_ENV.MQTT5_RSA_CERT,
-        test_env.AWS_IOT_ENV.MQTT5_RSA_KEY
-    );
+    constructor(options: TestingOptions) {
+        if (options.version == ProtocolVersion.Mqtt5) {
+            let builder = iot.AwsIotMqtt5ClientConfigBuilder.newDirectMqttBuilderWithMtlsFromPath(
+                test_env.AWS_IOT_ENV.MQTT5_HOST,
+                test_env.AWS_IOT_ENV.MQTT5_RSA_CERT,
+                test_env.AWS_IOT_ENV.MQTT5_RSA_KEY
+            );
 
-    builder.withConnectProperties({
-        clientId : uuid(),
-        keepAliveIntervalSeconds: 1200,
-    });
+            builder.withConnectProperties({
+                clientId : uuid(),
+                keepAliveIntervalSeconds: 1200,
+            });
 
-    let protocolClient = new mqtt5.Mqtt5Client(builder.build());
+            let protocolClient = new mqtt5.Mqtt5Client(builder.build());
 
-    if (testOptions.startConnected) {
-        let connectionSuccess = once(protocolClient, mqtt5.Mqtt5Client.CONNECTION_SUCCESS);
+            let rrOptions : mqtt_request_response.RequestResponseClientOptions = {
+                maxRequestResponseSubscriptions : 6,
+                maxStreamingSubscriptions : 2,
+                operationTimeoutInSeconds : 60,
+            }
 
-        protocolClient.start();
+            this.client = mqtt_request_response.RequestResponseClient.newFromMqtt5(protocolClient, rrOptions);
 
-        await connectionSuccess;
+            let connected = once(protocolClient, mqtt5.Mqtt5Client.CONNECTION_SUCCESS);
+            this.init = async () => {
+                protocolClient.start();
+
+                await connected;
+            };
+
+            let stopped = once(protocolClient, mqtt5.Mqtt5Client.STOPPED);
+            this.shutdown = async () => {
+                this.client.close();
+                protocolClient.stop();
+                await stopped;
+
+                protocolClient.close();
+            }
+        } else {
+            let builder = iot.AwsIotMqttConnectionConfigBuilder.new_mtls_builder_from_path(test_env.AWS_IOT_ENV.MQTT5_RSA_CERT, test_env.AWS_IOT_ENV.MQTT5_RSA_KEY);
+            builder.with_endpoint(test_env.AWS_IOT_ENV.MQTT5_HOST); // yes, 5 not 3
+            builder.with_client_id(uuid());
+
+            let client = new mqtt311.MqttClient();
+            let connection = client.new_connection(builder.build());
+
+            let rrOptions : mqtt_request_response.RequestResponseClientOptions = {
+                maxRequestResponseSubscriptions : 6,
+                maxStreamingSubscriptions : 2,
+                operationTimeoutInSeconds : 60,
+            }
+
+            this.client = mqtt_request_response.RequestResponseClient.newFromMqtt311(connection, rrOptions);
+
+            this.init = async () => {
+                await connection.connect();
+            };
+
+            this.shutdown = async () => {
+                this.client.close();
+                await connection.disconnect();
+            }
+        }
     }
 
-    let rrOptions : mqtt_request_response.RequestResponseClientOptions = {
-        maxRequestResponseSubscriptions : 6,
-        maxStreamingSubscriptions : 2,
-        operationTimeoutInSeconds : 60,
+    async open() {
+        await this.init();
     }
 
-    let rrClient = mqtt_request_response.RequestResponseClient.newFromMqtt5(protocolClient, rrOptions);
-
-    return {
-        protocolClient : protocolClient,
-        client : rrClient,
-    };
+    async close() {
+        await this.shutdown();
+    }
 }
 
-test('RequestResponseCreateDestroy', async () => {
-//test_env.conditional_test(test_env.AWS_IOT_ENV.mqtt5_is_valid_mtls_rsa())('RequestResponseCreateDestroy', async () => {
-    let context = await createMqtt5TestingContext({
-        startConnected : true
+test_env.conditional_test(test_env.AWS_IOT_ENV.mqtt5_is_valid_mtls_rsa())('Create Destroy Mqtt5', async () => {
+    let context = new TestingContext({
+        version: ProtocolVersion.Mqtt5
     });
+    await context.open();
 
-    context.client.close();
-
-    let stopped = once(context.protocolClient, mqtt5.Mqtt5Client.STOPPED);
-
-    context.protocolClient.stop();
-    await stopped;
-
-    context.protocolClient.close();
+    await context.close();
 });
 
-test('RequestResponseSuccess', async () => {
-//test_env.conditional_test(test_env.AWS_IOT_ENV.mqtt5_is_valid_mtls_rsa())('RequestResponseCreateDestroy', async () => {
-    io.enable_logging(io.LogLevel.TRACE);
-
-    let context = await createMqtt5TestingContext({
-        startConnected : true
+test_env.conditional_test(test_env.AWS_IOT_ENV.mqtt5_is_valid_mtls_rsa())('Create Destroy Mqtt311', async () => {
+    let context = new TestingContext({
+        version: ProtocolVersion.Mqtt311
     });
+    await context.open();
 
-    let correlationToken = uuid();
+    await context.close();
+});
 
-    let requestOptions : mqtt_request_response.RequestResponseOperationOptions= {
+function createRejectedGetNamedShadowRequest(addCorelationToken: boolean) : mqtt_request_response.RequestResponseOperationOptions {
+    let requestOptions : mqtt_request_response.RequestResponseOperationOptions = {
         subscriptionTopicFilters: [ "$aws/things/NoSuchThing/shadow/name/Derp/get/+" ],
         responsePaths: [{
+            topic: "$aws/things/NoSuchThing/shadow/name/Derp/get/accepted",
+        }, {
+            topic: "$aws/things/NoSuchThing/shadow/name/Derp/get/rejected",
+        }],
+        publishTopic: "$aws/things/NoSuchThing/shadow/name/Derp/get",
+        payload: `{}`,
+    }
+
+    if (addCorelationToken) {
+        let correlationToken = uuid();
+
+        requestOptions.responsePaths = [{
             topic: "$aws/things/NoSuchThing/shadow/name/Derp/get/accepted",
             correlationTokenJsonPath: "clientToken",
         }, {
             topic: "$aws/things/NoSuchThing/shadow/name/Derp/get/rejected",
             correlationTokenJsonPath: "clientToken",
-        }],
-        publishTopic: "$aws/things/NoSuchThing/shadow/name/Derp/get",
-        payload: `{\"clientToken\":\"${correlationToken}\"}`,
-        correlationToken: correlationToken,
+        }];
+        requestOptions.payload = `{\"clientToken\":\"${correlationToken}\"}`;
+        requestOptions.correlationToken = correlationToken;
     }
 
+    return requestOptions;
+}
+
+async function do_get_named_shadow_success_rejected_test(version: ProtocolVersion, useCorrelationToken: boolean) : Promise<void> {
+    let context = new TestingContext({
+        version: version
+    });
+
+    await context.open();
+
+    let requestOptions = createRejectedGetNamedShadowRequest(useCorrelationToken);
+
     let response = await context.client.submitRequest(requestOptions);
-    expect(response.topic).toEqual("$aws/things/NoSuchThing/shadow/name/Derp/get/rejected");
+    expect(response.topic).toEqual(requestOptions.responsePaths[1].topic);
     expect(response.payload.byteLength).toBeGreaterThan(0);
 
-    context.client.close();
+    await context.close();
+}
 
-    let stopped = once(context.protocolClient, mqtt5.Mqtt5Client.STOPPED);
+test_env.conditional_test(test_env.AWS_IOT_ENV.mqtt5_is_valid_mtls_rsa())('GetShadow Success Rejected Mqtt5', async () => {
+    await do_get_named_shadow_success_rejected_test(ProtocolVersion.Mqtt5, true);
+});
 
-    context.protocolClient.stop();
-    await stopped;
+test_env.conditional_test(test_env.AWS_IOT_ENV.mqtt5_is_valid_mtls_rsa())('GetShadow Success Rejected Mqtt311', async () => {
+    await do_get_named_shadow_success_rejected_test(ProtocolVersion.Mqtt311, true);
+});
 
-    context.protocolClient.close();
+test_env.conditional_test(test_env.AWS_IOT_ENV.mqtt5_is_valid_mtls_rsa())('GetShadow Success Rejected No CorrelationToken Mqtt5', async () => {
+    await do_get_named_shadow_success_rejected_test(ProtocolVersion.Mqtt5, false);
+});
+
+test_env.conditional_test(test_env.AWS_IOT_ENV.mqtt5_is_valid_mtls_rsa())('GetShadow Success Rejected No CorrelationToken Mqtt311', async () => {
+    await do_get_named_shadow_success_rejected_test(ProtocolVersion.Mqtt311, false);
 });
