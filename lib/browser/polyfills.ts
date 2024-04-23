abcf5beda184bc8953ef280d9f3e3de97d8fddcd
Zhihui Xia <zhvxia@amazon.com>
2024-04-23 11:14:00 -0700

    fix self window

--- lib/browser/polyfills.ts
+++ lib/browser/polyfills.ts
@@ -6,11 +6,14 @@ import buffer from 'buffer';
 import process from 'process';
 
 // Workaround to get mqtt-js working with Webpack 5
-if (typeof window !== 'undefined') {
-    (window as any).Buffer = buffer.Buffer;
-    (window as any).process = process;
-    // NodeJS global shim workaround for Angular
-    (window as any).global = window;
+if (typeof self !== 'undefined') {
+    (self as any).Buffer = buffer.Buffer;
+    (self as any).process = process;
+
+    if (self.window) {
+        // NodeJS global shim workaround for Angular
+        (self.window as any).global = window
+    }
 }
 
 export {};
