158e9729015e586faf9a3781711117839363d317
Bret Ambrose <bambrose@amazon.com>
2024-04-24 12:48:42 -0700

    Check in test work so that dependencies can be mucked with

--- lib/native/mqtt_request_response.spec.ts
+++ lib/native/mqtt_request_response.spec.ts
@@ -21,6 +21,7 @@ enum ProtocolVersion {
 
 interface TestingOptions {
     version: ProtocolVersion,
+    timeoutSeconds?: number,
 }
 
 class TestingContext {
@@ -48,7 +49,7 @@ class TestingContext {
             let rrOptions : mqtt_request_response.RequestResponseClientOptions = {
                 maxRequestResponseSubscriptions : 6,
                 maxStreamingSubscriptions : 2,
-                operationTimeoutInSeconds : 60,
+                operationTimeoutInSeconds : options.timeoutSeconds ?? 60,
             }
 
             this.client = mqtt_request_response.RequestResponseClient.newFromMqtt5(protocolClient, rrOptions);
@@ -167,18 +168,113 @@ async function do_get_named_shadow_success_rejected_test(version: ProtocolVersio
     await context.close();
 }
 
-test_env.conditional_test(test_env.AWS_IOT_ENV.mqtt5_is_valid_mtls_rsa())('GetShadow Success Rejected Mqtt5', async () => {
+test_env.conditional_test(test_env.AWS_IOT_ENV.mqtt5_is_valid_mtls_rsa())('GetNamedShadow Success Rejected Mqtt5', async () => {
     await do_get_named_shadow_success_rejected_test(ProtocolVersion.Mqtt5, true);
 });
 
-test_env.conditional_test(test_env.AWS_IOT_ENV.mqtt5_is_valid_mtls_rsa())('GetShadow Success Rejected Mqtt311', async () => {
+test_env.conditional_test(test_env.AWS_IOT_ENV.mqtt5_is_valid_mtls_rsa())('GetNamedShadow Success Rejected Mqtt311', async () => {
     await do_get_named_shadow_success_rejected_test(ProtocolVersion.Mqtt311, true);
 });
 
-test_env.conditional_test(test_env.AWS_IOT_ENV.mqtt5_is_valid_mtls_rsa())('GetShadow Success Rejected No CorrelationToken Mqtt5', async () => {
+test_env.conditional_test(test_env.AWS_IOT_ENV.mqtt5_is_valid_mtls_rsa())('GetNamedShadow Success Rejected No CorrelationToken Mqtt5', async () => {
     await do_get_named_shadow_success_rejected_test(ProtocolVersion.Mqtt5, false);
 });
 
-test_env.conditional_test(test_env.AWS_IOT_ENV.mqtt5_is_valid_mtls_rsa())('GetShadow Success Rejected No CorrelationToken Mqtt311', async () => {
+test_env.conditional_test(test_env.AWS_IOT_ENV.mqtt5_is_valid_mtls_rsa())('GetNamedShadow Success Rejected No CorrelationToken Mqtt311', async () => {
     await do_get_named_shadow_success_rejected_test(ProtocolVersion.Mqtt311, false);
+});
+
+function createAcceptedUpdateNamedShadowRequest(addCorelationToken: boolean) : mqtt_request_response.RequestResponseOperationOptions {
+    let requestOptions : mqtt_request_response.RequestResponseOperationOptions = {
+        subscriptionTopicFilters: [
+            "$aws/things/NoSuchThing/shadow/name/UpdateShadowCITest/update/accepted",
+            "$aws/things/NoSuchThing/shadow/name/UpdateShadowCITest/update/rejected"
+        ],
+        responsePaths: [{
+            topic: "$aws/things/NoSuchThing/shadow/name/UpdateShadowCITest/update/accepted",
+        }, {
+            topic: "$aws/things/NoSuchThing/shadow/name/UpdateShadowCITest/update/rejected",
+        }],
+        publishTopic: "$aws/things/NoSuchThing/shadow/name/UpdateShadowCITest/update",
+        payload: ``,
+    }
+
+    let desired_state = `{\"magic\":\"${uuid()}\"}`;
+
+    if (addCorelationToken) {
+        let correlationToken = uuid();
+
+        requestOptions.responsePaths[0].correlationTokenJsonPath = "clientToken";
+        requestOptions.responsePaths[1].correlationTokenJsonPath = "clientToken";
+        requestOptions.correlationToken = correlationToken;
+        requestOptions.payload = `{\"clientToken\":\"${correlationToken}\",\"state\":{\"desired\":${desired_state}}}`;
+    } else {
+        requestOptions.payload = `{\"state\":{\"desired\":${desired_state}}}`;
+    }
+
+    return requestOptions;
+}
+
+async function do_update_named_shadow_success_accepted_test(version: ProtocolVersion, useCorrelationToken: boolean) : Promise<void> {
+    let context = new TestingContext({
+        version: version
+    });
+
+    await context.open();
+
+    let requestOptions = createAcceptedUpdateNamedShadowRequest(useCorrelationToken);
+
+    let response = await context.client.submitRequest(requestOptions);
+    expect(response.topic).toEqual(requestOptions.responsePaths[0].topic);
+    expect(response.payload.byteLength).toBeGreaterThan(0);
+
+    await context.close();
+}
+
+test_env.conditional_test(test_env.AWS_IOT_ENV.mqtt5_is_valid_mtls_rsa())('UpdateNamedShadow Success Accepted Mqtt5', async () => {
+    await do_update_named_shadow_success_accepted_test(ProtocolVersion.Mqtt5, true);
+});
+
+test_env.conditional_test(test_env.AWS_IOT_ENV.mqtt5_is_valid_mtls_rsa())('UpdateNamedShadow Success Accepted Mqtt311', async () => {
+    await do_update_named_shadow_success_accepted_test(ProtocolVersion.Mqtt311, true);
+});
+
+test_env.conditional_test(test_env.AWS_IOT_ENV.mqtt5_is_valid_mtls_rsa())('UpdateNamedShadow Success Accepted No CorrelationToken Mqtt5', async () => {
+    await do_update_named_shadow_success_accepted_test(ProtocolVersion.Mqtt5, false);
+});
+
+test_env.conditional_test(test_env.AWS_IOT_ENV.mqtt5_is_valid_mtls_rsa())('UpdateNamedShadow Success Accepted No CorrelationToken Mqtt311', async () => {
+    await do_update_named_shadow_success_accepted_test(ProtocolVersion.Mqtt311, false);
+});
+
+async function do_get_named_shadow_failure_timeout_test(version: ProtocolVersion, useCorrelationToken: boolean) : Promise<void> {
+    let context = new TestingContext({
+        version: version,
+        timeoutSeconds: 4,
+    });
+
+    await context.open();
+
+    let requestOptions = createRejectedGetNamedShadowRequest(useCorrelationToken);
+    requestOptions.publishTopic = "not/the/right/topic";
+
+    await expect(context.client.submitRequest(requestOptions)).rejects.toBeDefined();
+
+    await context.close();
+}
+
+test_env.conditional_test(test_env.AWS_IOT_ENV.mqtt5_is_valid_mtls_rsa())('GetNamedShadow Failure Timeout Mqtt5', async () => {
+    await do_get_named_shadow_failure_timeout_test(ProtocolVersion.Mqtt5, true);
+});
+
+test_env.conditional_test(test_env.AWS_IOT_ENV.mqtt5_is_valid_mtls_rsa())('GetNamedShadow Failure Timeout Mqtt311', async () => {
+    await do_update_named_shadow_success_accepted_test(ProtocolVersion.Mqtt311, true);
+});
+
+test_env.conditional_test(test_env.AWS_IOT_ENV.mqtt5_is_valid_mtls_rsa())('GetNamedShadow Failure Timeout No CorrelationToken Mqtt5', async () => {
+    await do_update_named_shadow_success_accepted_test(ProtocolVersion.Mqtt5, false);
+});
+
+test_env.conditional_test(test_env.AWS_IOT_ENV.mqtt5_is_valid_mtls_rsa())('GetNamedShadow Failure Timeout No CorrelationToken Mqtt311', async () => {
+    await do_update_named_shadow_success_accepted_test(ProtocolVersion.Mqtt311, false);
 });
