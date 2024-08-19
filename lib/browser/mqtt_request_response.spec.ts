fae28664f6bab0cac2b1ea9f724a28488cd7c274
Bret Ambrose <bambrose@amazon.com>
2024-08-19 12:27:32 -0700

    Checkpoint

---
+++ lib/browser/mqtt_request_response.spec.ts
@@ -0,0 +1,13 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+import * as protocol_adapter_mock from "./mqtt_request_response/protocol_adapter_mock";
+import * as mqtt_request_response from "./mqtt_request_response";
+
+jest.setTimeout(10000);
+
+test('', () => {
+
+})
