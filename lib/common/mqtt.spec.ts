596c3a87961aaef3bfab3e2179f3d3f5d8efedde
Bret Ambrose <bambrose@amazon.com>
2024-11-18 13:33:24 -0800

    Use relative path

--- lib/common/mqtt.spec.ts
+++ lib/common/mqtt.spec.ts
@@ -198,7 +198,7 @@ test_env.conditional_test(test_env.AWS_IOT_ENV.mqtt311_is_valid_iot_cred())('MQT
     await onDisconnect;
 });
 
-import * as io from '@awscrt';
+import * as io from '../native/io';
 
 test_env.conditional_test(test_env.AWS_IOT_ENV.mqtt311_is_valid_iot_cred())('MQTT payload types', async () => {
 
