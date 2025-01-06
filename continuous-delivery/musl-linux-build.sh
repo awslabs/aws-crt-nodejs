fb5ae49183008fe04b5064581ff97054df1b8c7f
Dengke <dengket@amazon.com>
2025-01-06 14:39:59 -0800

    let's try to add ci using arm Mac

---
+++ continuous-delivery/musl-linux-build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -ex
+
+IMAGE_NAME=$1
+shift
+CLASSIFIER=$1
+shift
+
+# Pry the builder version this CRT is using out of ci.yml
+BUILDER_VERSION=$(cat .github/workflows/ci.yml | grep 'BUILDER_VERSION:' | sed 's/\s*BUILDER_VERSION:\s*\(.*\)/\1/')
+echo "Using builder version ${BUILDER_VERSION}"
+
+aws ecr get-login-password | docker login 123124136734.dkr.ecr.us-east-1.amazonaws.com -u AWS --password-stdin
+export DOCKER_IMAGE=123124136734.dkr.ecr.us-east-1.amazonaws.com/${IMAGE_NAME}:${BUILDER_VERSION}
+
+export BRANCH_TAG=$(git describe --tags)
+docker run --mount type=bind,src=$(pwd),dst=/root/aws-crt-nodejs --env AWS_ACCESS_KEY_ID --env AWS_SECRET_ACCESS_KEY --env AWS_DEFAULT_REGION --env CXXFLAGS --env AWS_CRT_ARCH $DOCKER_IMAGE --version=${BUILDER_VERSION} build -p aws-crt-nodejs --classifier ${CLASSIFIER} --branch ${BRANCH_TAG} run_tests=false
+docker container prune -f
