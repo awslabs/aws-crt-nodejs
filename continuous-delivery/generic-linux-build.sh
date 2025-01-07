16e1f428cf243e2cfd3fef21e6a7ec64cc605f46
Dengke <dengket@amazon.com>
2025-01-07 09:42:51 -0800

    remove unneeded things

--- continuous-delivery/generic-linux-build.sh
+++ continuous-delivery/generic-linux-build.sh
@@ -4,8 +4,6 @@ set -ex
 
 IMAGE_NAME=$1
 shift
-CLASSIFIER=$1
-shift
 
 # Pry the builder version this CRT is using out of ci.yml
 BUILDER_VERSION=$(cat .github/workflows/ci.yml | grep 'BUILDER_VERSION:' | sed 's/\s*BUILDER_VERSION:\s*\(.*\)/\1/')
@@ -15,5 +13,5 @@ aws ecr get-login-password | docker login 123124136734.dkr.ecr.us-east-1.amazona
 export DOCKER_IMAGE=123124136734.dkr.ecr.us-east-1.amazonaws.com/${IMAGE_NAME}:${BUILDER_VERSION}
 
 export BRANCH_TAG=$(git describe --tags)
-docker run --mount type=bind,src=$(pwd),dst=/root/aws-crt-nodejs --env CXXFLAGS --env AWS_CRT_ARCH $DOCKER_IMAGE --version=${BUILDER_VERSION} build -p aws-crt-nodejs --classifier ${CLASSIFIER} --branch ${BRANCH_TAG} run_tests=false
+docker run --mount type=bind,src=$(pwd),dst=/root/aws-crt-nodejs --env CXXFLAGS $DOCKER_IMAGE --version=${BUILDER_VERSION} build -p aws-crt-nodejs --branch ${BRANCH_TAG} run_tests=false
 docker container prune -f
