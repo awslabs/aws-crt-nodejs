5fd9b059152296ca373f034104219f8a5fa0935b
Dengke Tang <dengket@amazon.com>
2025-05-05 13:10:33 -0700

    take 2

--- test/browser/jest.setup.js
+++
@@ -1,8 +0,0 @@
-const { setup: setupPuppeteer } = require('jest-environment-puppeteer');
-/**
- * Sets up the environment for running tests with Jest + Puppeteer
- */
-module.exports = async function globalSetup(globalConfig) {
-    process.env.CI = true
-    await setupPuppeteer(globalConfig);
-};
