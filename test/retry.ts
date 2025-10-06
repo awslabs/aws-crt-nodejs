83fe45c1332f733340646c4d1cfb4682a518904b
Bret Ambrose <bretambrose@gmail.com>
2025-10-06 11:38:11 -0700

    Additional test retry updates uncovered by CI (#649)
    Co-authored-by: Bret Ambrose <bambrose@amazon.com>

--- test/retry.ts
+++ test/retry.ts
@@ -6,7 +6,9 @@
 // TODO: browser connection timeouts will have different error messages, but we need to wait for them to occur in order
 // to find out what they are
 function isTimeout(e : Error) {
-    return e.message.includes("AWS_IO_TLS_NEGOTIATION_TIMEOUT") || e.message.includes("AWS_IO_SOCKET_TIMEOUT");
+    return e.message.includes("AWS_IO_TLS_NEGOTIATION_TIMEOUT") ||
+        e.message.includes("AWS_IO_SOCKET_TIMEOUT") ||
+        e.message.includes("AWS_IO_DNS_INVALID_NAME");
 }
 
 export async function basicRetryWrapper(maxAttempts: number, isRetryablePredicate: (err: Error) => boolean, testFunction: () => Promise<void>) : Promise<void> {
@@ -20,6 +22,9 @@ export async function basicRetryWrapper(maxAttempts: number, isRetryablePredicat
             if (!isRetryablePredicate(e as Error) || attempt >= maxAttempts) {
                 throw e;
             }
+
+            // pause for a second before retrying
+            await new Promise((resolve) => setTimeout(resolve, 1000));
         }
     }
 }
