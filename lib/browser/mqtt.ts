776709990e8fee02e55e9aab593cc55eb0dc2ab0
Bret Ambrose <bambrose@amazon.com>
2026-03-18 13:44:14 -0700

    Formatting

--- lib/browser/mqtt.ts
+++ lib/browser/mqtt.ts
@@ -12,7 +12,6 @@
  * @mergeTarget
  */
 
-// import * as mqtt from "mqtt";
 import * as internal_mqtt_client from "./mqtt_internal/client";
 import * as mqtt5 from "../common/mqtt5";
 import * as ws from "./ws"
@@ -375,7 +374,8 @@ export class MqttClientConnection extends BufferedEventEmitter {
             // ideally, we'd use the socket options timeout value here
             connectTimeoutMillis: this.config.ping_timeout ? this.config.ping_timeout : 30 * 1000,
             retryJitterMode: mqtt5.RetryJitterType.Default,
-            resubscribeMode: internal_mqtt_client.ResubscribeModeType.EnabledAlways // this was how the previous mqtt-js integration behaved
+            // this was how the previous mqtt-js integration behaved
+            resubscribeMode: internal_mqtt_client.ResubscribeModeType.EnabledAlways
         };
 
         if (config.reconnect_min_sec !== undefined) {
